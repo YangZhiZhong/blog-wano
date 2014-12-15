05541a5834d0b61c3d8dca6c4559ae121809ecaa
Izotov Ivan <ivanizotov2@gmail.com>
2014-12-16 01:44:20 +0300

    clear config

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -1,16 +1,13 @@
 <?php
+
 return [
-    'components' => [
-        'db' => [
-            'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
-            'username' => 'root',
-            'password' => '',
-            'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-        ],
-    ],
+  'components' => [
+    'db' => [
+      'class' => 'yii\db\Connection',
+      'dsn' => 'mysql:host=localhost;dbname=blog',
+      'username' => 'root',
+      'password' => '55555',
+      'charset' => 'utf8',
+    ]
+  ],
 ];
